58f9e3b807bcfbe3b4579cffc940e189d5790948
roberacc <roberacc@s2pc40.eisc.univalle.edu.co>
2012-12-12 18:31:50 -0500

    update

--- src/the_codificador/MillerRabin.java
+++ src/the_codificador/MillerRabin.java
@@ -12,12 +12,15 @@ import javax.swing.JOptionPane;
  */
 public class MillerRabin {
 
-    
     static long primo1 = 0;
     static long primo2 = 0;
+    static long z = 0;
+    static long f = 0;
+    static long n = 0;
+    static long s = 0;
 
     private static int modular_exponent(long base, long power, long modulus) {
-        
+
         long result = 1;
         for (int i = 31; i >= 0; i--) {
             result = (result * result) % modulus;
@@ -51,7 +54,7 @@ public class MillerRabin {
     public static boolean miller_rabin(long n) {
         if (n <= 2) {
             return false;
-        }else if (miller_rabin_pass(2, n)
+        } else if (miller_rabin_pass(2, n)
                 && (n <= 7 || miller_rabin_pass(7, n))
                 && (n <= 61 || miller_rabin_pass(61, n))) {
             return true;
@@ -59,22 +62,22 @@ public class MillerRabin {
             return false;
         }
     }
-    
-    public static long gen_Primo(){
+
+    public static long gen_Primo() {
         boolean found = false;
         long primoA = 0;
         int c = 1;
-        
+
         primoA = (long) (Math.random() * 9000000 + 1000000);
-        
+
         while (!found) {
-            System.out.println("verificar: " + (primoA + c));
+            //System.out.println("verificar: " + (primoA + c));
 
             found = miller_rabin(primoA + c);
             //System.out.println(found);
             if (found) {
-                primoA = primoA+c;
-                System.out.println(primoA );
+                primoA = primoA + c;
+                System.out.println(primoA);
             }
 
             c++;
@@ -82,29 +85,73 @@ public class MillerRabin {
         return primoA;
     }
 
-    
-    public static long gen_Clave(){
-        
-        long n=primo1*primo2;
-        
-        System.out.println("producto"+n);
-        return n;
+    public static void gen_Clave() {
+
+        z = primo1 * primo2;
+        f = (primo1 - 1) * (primo2 - 1);
+        System.out.println("producto    " + z);
+        System.out.println("producto    " + f);
+
+        do {
+            n = (long) (Math.random() * (z - 2) + 1);
+            
+        } while (co_Primos(n, f) || det_S(n, z));
+
+        System.out.println("valor de n: " + n);
+        det_S(n,z);
+
+
     }
-    
+
+    public static boolean co_Primos(long a, long b) {
+        boolean found = true;
+
+        for (int i = 0; i < b; i++) {
+            long t = (long) (a * i);
+
+            if (t > b) {
+                found = false;
+                break;
+            } else if (t == b) {
+                found = true;
+                break;
+            }
+
+        }
+        
+
+        return found;
+    }
+
+    public static boolean det_S(long a, long b) {
+        boolean found=true;
+        System.out.println("valor ");
+        for (int i = 0; i < 100; i++) {
+            if ((a * i) % b == 1) {
+                s = i;
+                found = false;
+                System.out.println("valor de s: "+s);
+                break;
+            }
+        }
+        return found;
+    }
+
     public static void main(String[] args) {
         long n = Long.parseLong(JOptionPane.showInputDialog(null, "Numero"));
-        
+
         System.out.println(miller_rabin(n) ? n + " Es Primo" : n + " Es Compuesto");
         primo1 = gen_Primo();
         primo2 = gen_Primo();
+
         gen_Clave();
-        
-        if(miller_rabin(n)){
-        JOptionPane.showMessageDialog(null,"El numero: "+n+" es un numero Primo"  );
-        }else{
-            JOptionPane.showMessageDialog(null,"El numero: "+n+" es un numero Compuesto"  );
+
+        if (miller_rabin(n)) {
+            JOptionPane.showMessageDialog(null, "El numero: " + n + " es un numero Primo");
+        } else {
+            JOptionPane.showMessageDialog(null, "El numero: " + n + " es un numero Compuesto");
         }
-        JOptionPane.showMessageDialog(null, "Primo 1: "+primo1+"\nPrimo 2: "+primo2);
+        JOptionPane.showMessageDialog(null, "Primo 1: " + primo1 + "\nPrimo 2: " + primo2);
 
     }
 }
--- src/the_codificador/NewClass.java
+++ src/the_codificador/NewClass.java
@@ -16,28 +16,54 @@ public class NewClass {
     String msj="";
     char dato='a';
     static int datN=0;
-    BigInteger n, q, p;
+    static  long n, q, p;
+    static long z=4163171;
     static int tamPrimo=1244;
     
     public NewClass(){
-        generaPrimos();
+        //generaPrimos();
+        co_Primos(23,4868833);
         
         
     }
     
-    public void generaPrimos()
-    {
-        p = new BigInteger(tamPrimo, 10, new Random());
-        System.out.println("valor p: "+p);
-        do q = new BigInteger(tamPrimo, 10, new Random());
-            while(q.compareTo(p)==0);
+    public static boolean co_Primos(long a, long b) {
+        boolean found = true;
+
+        for (int i = 0; i < b; i++) {
+            long t = (long) (a * i);
+
+            if (t > b) {
+                found = true;
+                break;
+            } else if (t == b) {
+                found = false;
+                break;
+            }
+
+            return found;
+        }
+
+
+        return found;
     }
+    
+//    public void generaPrimos()
+//    {
+//        p = 3;
+//        System.out.println("valor p: "+p);
+//        do q = 13;
+//            while(q.compareTo(p)==0);
+//    }
     
     public static void main(String []args){
         
        datN =(int) 'A';
        System.out.println("codigo ASCII de la letra a: "+datN);
-       NewClass obj = new NewClass();
+       int p = (int) (Math.random()*20 +1);
+       n = (long) (Math.random() * (z - 2) + 1);
+       System.out.println("nnnnnnnn"+n+"ppppp"+p);
+       //NewClass obj = new NewClass();
        
        
        
